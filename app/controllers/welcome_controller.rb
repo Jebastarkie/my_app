962d0339261e859447a5f47ca3fdb50aa9d4e1af
Jeba Starkie <jeba.starkie@digital.cabinet-office.gov.uk>
2015-05-07 10:42:46 +0100

    added links to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,13 @@ class WelcomeController < ApplicationController
 	def hellopage
 	end
 	def page2
+	end
+	def webapppage
+	end
+	def gitpage
+	end
+	def herokupage
+	end
+	def autotestpage
 	end	
 end
